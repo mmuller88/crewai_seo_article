b3687b5433a1c36438d449d49a316793951e7bf9
Martin Mueller <damadden88@googlemail.com>
2025-03-02 16:43:36 +0000

    move agents and tasks to yaml file

--- src/crewai_seo_article/crew.py
+++ src/crewai_seo_article/crew.py
@@ -30,44 +30,19 @@ class CrewaiSeoArticle():
         file_paths=["article_content_writer_temperatur_empfehlung.md"]
     )
 
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
-    # @agent
-    # def researcher(self) -> Agent:
-    # 	return Agent(
-    # 		config=self.agents_config['researcher'],
-    # 		verbose=True
-    # 	)
-
-    # @agent
-    # def reporting_analyst(self) -> Agent:
-    # 	return Agent(
-    # 		config=self.agents_config['reporting_analyst'],
-    # 		verbose=True
-    # 	)
-
     @agent
     def article_content_writer(self) -> Agent:
-
         return Agent(
-            role="Produktbeschreiber",
-            goal="Erstelle präzise und ansprechende Produktbeschreibungen, die alle wichtigen Eigenschaften des Kleidungsstücks verständlich vermitteln.",
-            backstory="Sie sind ein erfahrener Texter im Bereich Mode und Textilien mit einem ausgeprägten Gespür für Materialien und Verarbeitung. Bekannt für Ihre Fähigkeit, technische Eigenschaften in verständliche und ansprechende Produktbeschreibungen zu übersetzen, die sowohl informativ als auch verkaufsfördernd sind.",
-            # config=self.agents_config['article_content_writer'],
+            config=self.agents_config['article_content_writer'],
             verbose=True,
-            # tools=[search_tool],
             knowledge_sources=[self.text_source]
         )
 
     @agent
     def also_asked_researcher(self) -> Agent:
         search_tool = SerperDevTool()
-
         return Agent(
-            role="Auch gefragt Fragebauer",
-            goal="Finde heraus, welche Fragen die Nutzer häufiger stellen und welche Themen sie interessieren.",
-            # config=self.agents_config['article_content_writer'],
-            backstory="Sie sind ein erfahrener Fragebauer und haben ein großes Interesse an den Bedürfnissen und Wünschen der Nutzer. Sie sind ein Experte für das Finden von Informationen und haben ein gutes Gespür für die Bedürfnisse der Nutzer.",
+            config=self.agents_config['also_asked_researcher'],
             verbose=True,
             tools=[search_tool],
             knowledge_sources=[self.text_source]
@@ -75,31 +50,11 @@ class CrewaiSeoArticle():
     
     @agent
     def also_asked_answerer(self) -> Agent:
-
         return Agent(
-            role="Auch gefragt Antworter",
-            goal="Antworte auf die Fragen, die die Nutzer häufiger stellen.",
-            # config=self.agents_config['article_content_writer'],
-            backstory="Sie sind ein erfahrener Antworter und haben ein großes Interesse an den Bedürfnissen und Wünschen der Nutzer. Sie sind ein Experte für das Finden von Informationen und haben ein gutes Gespür für die Bedürfnisse der Nutzer.",
+            config=self.agents_config['also_asked_answerer'],
             verbose=True,
             knowledge_sources=[self.text_source]
         )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-    # @task
-    # def research_task(self) -> Task:
-    # 	return Task(
-    # 		config=self.tasks_config['research_task'],
-    # 	)
-
-    # @task
-    # def reporting_task(self) -> Task:
-    # 	return Task(
-    # 		config=self.tasks_config['reporting_task'],
-    # 		output_file='report.md'
-    # 	)
 
     @task
     def article_content_task(self) -> Task:
@@ -111,21 +66,13 @@ class CrewaiSeoArticle():
     @task
     def also_asked_task(self) -> Task:
         return Task(
-            config={
-                'description': "Finde die fünf häufigsten Fragen heraus, welche die Nutzer stellen und welche Themen sie interessieren im Zusammenhang mit dem Produkt.",
-                'expected_output': "Eine Liste von Fragen und Themen, die die Nutzer häufiger stellen und interessieren.",
-                'agent': self.also_asked_researcher()
-            }
+            config=self.tasks_config['also_asked_task']
         )
 
     @task
     def also_asked_answerer_task(self) -> Task:
         return Task(
-            config={
-                'description': "Antworte auf die Fragen, die die Nutzer häufiger stellen.",
-                'expected_output': "Die Produktbeschreibung und eine Liste von Fragen und Antworten.",
-                'agent': self.also_asked_answerer()
-            }
+            config=self.tasks_config['also_asked_answerer_task']
         )
 
     @crew
